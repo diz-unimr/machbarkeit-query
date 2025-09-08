cb2c8c7676bcb6dd2045874c981ff578ac8df882
Sebastian Stöcker <sebastian.stoecker@uni-marburg.de>
2025-09-08 13:15:59 +0200

    feat: support oauth client credentials auth flow for the broker connection

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
+mod auth;
 mod client;
 mod config;
 mod model;
 
+use crate::auth::{TokenService, TokenServiceConfig};
 use crate::client::RestClient;
 use crate::config::AppConfig;
 use crate::model::FeasibilityRequest;
 use crate::model::QueryState::Completed;
+use anyhow::anyhow;
 use futures_util::{SinkExt, StreamExt};
 use log::{debug, error, info};
+use reqwest::header::{HeaderValue, AUTHORIZATION};
 use reqwest::StatusCode;
 use tokio::net::TcpStream;
+use tokio_tungstenite::tungstenite::client::IntoClientRequest;
 use tokio_tungstenite::{
     connect_async, tungstenite::protocol::Message, MaybeTlsStream, WebSocketStream,
 };
 use tracing_subscriber::EnvFilter;
-use url::Url;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
@@ -28,16 +32,50 @@ async fn main() -> Result<(), anyhow::Error> {
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::try_from_default_env().unwrap_or_else(|_| filter.into()))
         .init();
+
+    // http client
     let client = RestClient::new(&config.feasibility)?;
 
-    let url = Url::parse(config.broker.url.as_str())?;
-    let (ws_stream, _) = connect_async(url.as_str())
+    // auth token service
+    let token_service = match &config.broker.auth.map(|a| a.client_credentials).flatten() {
+        None => None,
+        Some(auth) => {
+            // token service
+            let token_config = TokenServiceConfig {
+                token_url: auth.clone().token_url,
+                client_id: auth.client_id.clone(),
+                client_secret: auth.client_secret.clone(),
+            };
+
+            // Create a new token service instance
+            Some(TokenService::new(token_config))
+        }
+    };
+    let mut request = config.broker.url.as_str().into_client_request()?;
+    if let Some(service) = token_service {
+        request.headers_mut().insert(
+            AUTHORIZATION,
+            HeaderValue::from_str(
+                format!(
+                    "Bearer {}",
+                    service
+                        .get_token()
+                        .await
+                        .map(|t| t.secret().to_string())
+                        .map_err(|e| anyhow!("Failed to open file: {}", e))?
+                )
+                .as_str(),
+            )?,
+        );
+    };
+
+    let (ws_stream, _) = connect_async(request.clone())
         .await
         .expect("Failed to connect");
     info!("WebSocket client connected");
 
     // read incoming messages
-    info!("Reading messages from {}", url);
+    info!("Reading messages from {}", request.uri());
     tokio::spawn(ws_read(ws_stream, client)).await?;
 
     Ok(())
